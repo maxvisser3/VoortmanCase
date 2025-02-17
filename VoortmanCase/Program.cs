5a6779fe343096a3235cfdb2efe8350539ed1063
maxvisser3 <m.visser-3@student.utwente.nl>
2025-02-17 11:15:52 +0100

    Add project files.

---
+++ VoortmanCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.IO;
+
+namespace VoortmanCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<string> files = new List<string>();
+            files.Add("socker.dat");
+            files.Add("temperature.dat");
+
+            //folderPath = ""
+
+
+
+        }
+
+        static List<DataTable> readDataFiles(List<string> filePaths)
+        {
+
+            List<DataTable> dataTables = new List<DataTable>();
+
+            return dataTables;
+        }
+
+    }
+}
